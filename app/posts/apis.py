f1be636071187ac5cc6a6e2f01d7a3464eb06310
이한영 <dev@lhy.kr>
2020-02-18 16:00:21 +0900

    PostListCreateAPIView의 GET(list)요청 구현

--- app/posts/apis.py
+++ app/posts/apis.py
@@ -1,7 +1,12 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from .models import Post
+from .serializers import PostSerializer
+
 
 class PostListCreateAPIView(APIView):
     def get(self, request):
-        return Response('abc')
+        posts = Post.objects.all()
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
